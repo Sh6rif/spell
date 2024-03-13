957ed4e538cee8da65531fec2a31430e8f70615c
Sherif Alaa <sherefalex34@gmail.com>
2024-03-13 06:06:20 +0200

    up8

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update packages
+apt-get update
+
+# Install Python (if not already installed)
+apt-get install -y python3 python3-pip
+
+# Install autocorrect module
+pip3 install autocorrect
+
+# Install Node.js dependencies
+npm install
+
+# Run your application
+node index.js
